9e96e93af841e71bc9b81ac4fb6cc5d556e637e1
Sandra Muraca <sandramuraca78@gmail.com>
2021-06-30 21:57:31 -0300

    clase 21 - 25 - 26

---
+++ 25.intro-javascriptMalena/app.js
@@ -0,0 +1,11 @@
+/* Las ordenes en javascript se pueden escribir una etiqueta script en el body o linkear un archivo JS
+
+el archivo de hmtl tiene que tener el mismo nombre que el JS
+no poner nombres distintos
+
+*/
+
+//variable: espacio de memoria
+// no puedo declarar una variable 2 veces con el mismo nombre, si puedo reasignar un valor a esa variable
+
+let nombre = "Sandra";
