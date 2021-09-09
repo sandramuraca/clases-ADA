40774db7a239fd98cfcfea15a9f7fd4e957ef591
Sandra Muraca <sandramuraca78@gmail.com>
2021-09-08 21:05:46 -0300

    clase 52 y 53

---
+++ 52.53.matrices-generarGrilla/app.js
@@ -0,0 +1,73 @@
+//RESUMEN:
+//           *METODOS:
+//           *Math.random()
+//           *Math.floor()
+//           *Math.ceil()
+//           *Math.round()
+//           *Nro al azar dentro de un rango
+//           *match 3 - grilla de emojis random / Mumuki Matrices 4           Generar Grilla  --> en archivo 
+
+
+
+//MVP --> Minimum Viable Project
+//cuando tenemos un proyecto grande hay que ir  armando de a poco cosas pequeñas y que funcionen
+
+
+//Math.random() me da un numero al azar entre 0 y 1 (con muchos decimales)
+console.log(Math.random())
+
+//Math.floor() --> piso
+console.log(Math.floor(5.9))
+
+//Math.ceil() --> techo
+console.log(Math.ceil(5.9))
+
+//Math.round() --> redondeo desde el medio
+console.log(Math.round(5.9))
+console.log(Math.round(5.3))
+
+//Un numero al zar de 0 a 10 (10 no inclusive)
+const numeroRandom1 = Math.floor(Math.random()*10)
+console.log(numeroRandom1)
+
+//Un numero al zar de 0 a 100 (100 no inclusive)
+const numeroRandom2 = Math.floor(Math.random()*100)
+console.log(numeroRandom2)
+
+//Un numero al zar de 0 a 3 (13 no inclusive)
+const numeroRandom3 = Math.floor(Math.random()*3)
+console.log(numeroRandom3)
+
+
+
+//funcion auxiliar
+const obtenerNumeroAlAzar = () =>{
+    numeroAlAzar = Math.floor(Math.random()*10)
+    return numeroAlAzar
+}
+
+console.log(obtenerNumeroAlAzar())
+
+//funcion que genera emijis al azar pasados dentro de un array
+const arr = ["🍋", "🍓", "🥝", "🍇", "🍌"] //array.length=5
+const arrayLargo = ["🍺", "🍦", "🍸", "🥂", "🍰", "🍧", "🍩", "🍨", "🍹"] //array.length=9
+
+const obtenerNumeroAlAzar2 = (array) => {
+    numeroAlAzar = Math.floor(Math.random() * array.length) //devuelve un nro al azar entre 0 y el numero que representa array.length (5 o 9 segun los ejemplos)
+    return numeroAlAzar
+}
+
+console.log(obtenerNumeroAlAzar2(arr))
+console.log(obtenerNumeroAlAzar2(arrayLargo))
+
+//esa funcion se puede escribir tambien de esta forma:
+const obtenerNumeroAlAzar3 = (array) => {
+    return Math.floor(Math.random() * array.length) //devuelve un nro al azar entre 0 y el numero que representa array.length (5 o 9 segun los ejemplos)
+  
+}
+
+const numeroAlAzarCorto = obtenerNumeroAlAzar3(arr)
+const numeroAlAzarLargo = obtenerNumeroAlAzar3(arrayLargo)
+
+console.log(numeroAlAzarCorto)
+console.log(numeroAlAzarLargo)
---
+++ 52.53.matrices-generarGrilla/generarGrilla/generarGrilla.js
@@ -0,0 +1,77 @@
+//Ejercicio Mumuki
+//dada una matriz, llevar esa matriz a html en forma de tabla
+//generar una grilla con elementos al azar
+//Problemas a resolver:
+//como elijo un elemento al azar?
+// como agrego ese elemento a otro array?
+//como genero una grilla?
+//como genero una grilla con x cantidad de filas
+//como genero una grilla con x cantidad de columnas?
+
+
+//---FUNCION PARA GENERAR NUMEROS AL AZAR---como devuelvo emojis (numeros) al azar? --> en ese caso los emojis son numeros, que a su vez son las posiciones del array
+//esta funcion la voy a reutilizar como funcion auxiliar
+
+const arr = ["🍋", "🍓", "🥝", "🍇", "🍌"] //array.length=5
+const arrayLargo = ["🍺", "🍦", "🍸", "🥂", "🍰", "🍧", "🍩", "🍨", "🍹"] //array.length=9
+
+const obtenerNumeroAlAzar = (array) => {
+    return Math.floor(Math.random() * array.length) //devuelve un nro al azar entre 0 y el numero que representa array.length (5 o 9 segun los ejemplos)
+    
+}
+
+//en la variable -numeroAlAzar- guardo el nro que me genera la ejecucion de la funcion -obtenerNumeroAlAzar-, cuando llamo la funcion le paso como parametro el array que quiero que tome en cuenta para generar un numero al azar 
+let numeroAlAzar = (obtenerNumeroAlAzar(arr))
+console.log(numeroAlAzar)
+
+//let numeroAlAzar = (obtenerNumeroAlAzar(arrayLargo))
+//console.log(numeroAlAzar)
+
+
+//--FUNCION PARA GENERAR ARRAY CON NUMEROS AL AZAR ---> los numeros al azar fueron generados en la funcion anterior,*obtenerNumeroAlAzar* utilizada como auxiliar. Esos números se van a ser la posicion del array, y de esa posición pusheo el elemento, en este caso es un emoji
+const generarArrayAlAzar =(columnas, items) =>{
+    
+    let resultado = []
+
+    for (let i = 0; i < columnas; i++) {
+        //emojiAlAzar es la variable donde guardo el numero al azar que me retorna la funcion obtenerNumeroAlAzar = es un numero
+        const emojiAlAzar = obtenerNumeroAlAzar(items) //items= array que paso en parametro
+        resultado.push(items[emojiAlAzar])
+        
+    }
+    return resultado
+}
+
+const arrayGatos = ["🐱","❤", "🐟", "📦", "🐈", "🧶"]
+console.log(generarArrayAlAzar(5, arrayGatos))
+
+
+//---FUNCIONA PARA GENERAR UNA MATRIZ---
+//problemas a resolver:
+//crear una funcion que reciba 3 parametros: filas, columnas, items
+//generar arrays vacios (?) y pushearlos dentro de la variable "resultado" declarada dentro de la funcion como acumuladora
+//la cantidad de arrays vacios a pushear estara definida por el parametro "Filas"
+//ir pusheando dentro de esos arrays vacios elementos ramdom
+//la cantidad de elementos a pushear estara definida por el parámetro "columna"
+//los elementos a pushear estarán definimos por el parametro "item"
+
+
+const generarGrilla = (filas, columnas, items) =>{
+    let matriz= []
+   
+    for (let i = 0; i < filas; i++) {
+        const arrayAlAzar = generarArrayAlAzar(columnas, items)
+        //genero cantidad de columnas que le paso por parametro e items que tambien le paso por parametro
+        matriz.push(arrayAlAzar)
+    }
+    return matriz
+}
+
+console.log(generarGrilla(2, 3, [1, 2]))
+
+//posible resultado
+// // [
+//   ['c', 'c', 'a'], 
+//   ['c', 'a', 'a'], 
+//   
+// ]
