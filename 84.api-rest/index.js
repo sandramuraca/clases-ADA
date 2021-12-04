8b17114d9fcc94c1bffa3bdd02222fe0b5985279
Sandra Muraca <sandramuraca78@gmail.com>
2021-12-03 22:00:22 -0300

    pendientes editar

--- 84.api-rest/index.js
+++ 84.api-rest/index.js
@@ -32,6 +32,31 @@ const borrarUsuario = (id) =>{
   })
 }
 
+const crearBotonesEditar = () =>{
+  const botonesEditar = document.querySelectorAll(".boton-editar");
+
+  for (let i = 0; i < botonesEditar.length; i++) {
+      botonesEditar[i].onclick = () =>{
+          const idUsuarioAEditar = botonesEditar[i].dataset.id;
+          editarUsuario(idUsuarioAEditar);//***ojo no esta creada la funcion!!!***
+      }
+  }
+}
+
+const editarUsuario = (id) =>{
+  fetch(`https://601da02bbe5f340017a19d60.mockapi.io/users/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify(datosUsuarioEditar), //****ojo esta variable no esta declarada!!!****
+      headers: {
+    "Content-Type": "application/json"
+  }
+  })
+  .then((res) => res.json())
+  .then((data) =>{
+  recuperarDatosApi();
+  })
+}
+
 
 /*******Get para traer datos de mi Api**********/
 const recuperarDatosApi= () =>{
@@ -44,6 +69,13 @@ const recuperarDatosApi= () =>{
 
 recuperarDatosApi()
 
+//PENDIENTE
+//abrir formulario editar cuando se presione el icono de editar
+//recuperar los datos en el formulario en los campos correspondientes del usuario que quiero editar
+//cuando presiono el boton editar usuario ejecuto el metodo patch y actualizo la api
+//ejecuto el get nuevamente para voolver a actualizar el html
+
+
 /*******Crear tabla con los datos de la Api**********/
 const crearTablaHTML = (data) => {
   const tabla = document.querySelector("#tabla")
@@ -54,7 +86,7 @@ const crearTablaHTML = (data) => {
       <td>${curr.email}</td>
       <td>${curr.address}</td>
       <td>${curr.phone}</td>
-      <td><button data-id="${curr.id}"><i class="fas fa-pencil-alt"></i>
+      <td><button class= "boton-editar" data-id="${curr.id}"><i class="fas fa-pencil-alt"></i>
       </button></td>
       <td><button class= "boton-borrar" data-id="${curr.id}"><i class="fas fa-trash-alt"></i>
       </button></td>
@@ -71,7 +103,8 @@ const crearTablaHTML = (data) => {
     `)
 
     tabla.innerHTML = html
-    crearBotonesBorrar() //funcion para seleccionar los botones
+    crearBotonesBorrar() //funcion para seleccionar los botones de borrado
+    crearBotonesEditar() //funcion para seleccionar los botones de editar
 }
 
 /*******Boton para abrir el formulario para crear nuevo usuario**********/
