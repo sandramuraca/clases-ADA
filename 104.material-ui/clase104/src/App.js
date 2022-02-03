a37d645ca8af01c61fb787ee934117191166c34a
Sandra Muraca <sandramuraca78@gmail.com>
2022-02-03 00:14:04 -0300

    arreglo margenes

--- 104.material-ui/clase104/src/App.js
+++ 104.material-ui/clase104/src/App.js
@@ -60,80 +60,82 @@ function App() {
         <h2>Ejercicios clase</h2>
         <h3>Segunda card</h3>
 
-      <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image="http://www.placekitten.com/200"
-        alt="gatito"
-      />
-      <CardContent>
-        <Typography variant="h6">
-          Our changing Planet
-        </Typography>
-        <Typography  gutterBottom variant="body1" color="text.secondary">
-          By Kurt Wagner
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        </Typography>
-
-      </CardContent>
-      <CardActions>
-        <Button size="medium" color="secondary">Read</Button>
-        <Button size="medium" color="secondary">Bookmark</Button>
-        <IconButton>
-          <FavoriteIcon></FavoriteIcon>
-        </IconButton>
-        <IconButton>
-          <ShareIcon></ShareIcon>
-        </IconButton>
-      </CardActions>
-    </Card>
-      
-    <h3>Tercera card</h3>
-    <Card sx={{ maxWidth: 345 }}>
-      
-      <CardMedia
-        component="img"
-        height="194"
-        image="http://www.placekitten.com/300"
-        alt="gatito"
-      />
-      <CardContent>
-        <Typography variant="h5">
-          Gatito Mirando
-        </Typography>
-        <Rating
-        name="simple-controlled"
-        value={4.5}
-        precision={0.5}
+      <Box sx={{ mx: "auto", width: 400, mb:6 }}
+      > 
+        <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image="http://www.placekitten.com/200"
+          alt="gatito"
         />
-        <Typography variant="body2" color="text.rprimary" fontWeight="medium" gutterBottom>
-          $ ralladito - no esta a la venta
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. 
-        </Typography>
-        <hr color="#cecccc"></hr>
-        <Typography variant="body2" color="text.rprimary" fontWeight="medium"  gutterBottom>
-          Dias de visita
-        </Typography>
-      </CardContent>
-      
-      
-      <CardActions>
-       <Chip label="Lunes" color="secondary" />
-       <Chip label="Martes" />
-       <Chip label="Miercoles" />
-       <Chip label="Jueves" />
-      </CardActions>
-        
-      <Button  color="secondary">Reserve</Button>
-         
-    </Card>
+        <CardContent>
+          <Typography variant="h6">
+            Our changing Planet
+          </Typography>
+          <Typography  gutterBottom variant="body1" color="text.secondary">
+            By Kurt Wagner
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          </Typography>
 
+        </CardContent>
+        <CardActions>
+          <Button size="medium" color="secondary">Read</Button>
+          <Button size="medium" color="secondary">Bookmark</Button>
+          <IconButton>
+            <FavoriteIcon></FavoriteIcon>
+          </IconButton>
+          <IconButton>
+            <ShareIcon></ShareIcon>
+          </IconButton>
+        </CardActions>
+      </Card>
+        
+      <h3>Tercera card</h3>
+      <Card sx={{ maxWidth: 345 }}>
+        
+        <CardMedia
+          component="img"
+          height="194"
+          image="http://www.placekitten.com/300"
+          alt="gatito"
+        />
+        <CardContent>
+          <Typography variant="h5">
+            Gatito Mirando
+          </Typography>
+          <Rating
+          name="simple-controlled"
+          value={4.5}
+          precision={0.5}
+          />
+          <Typography variant="body2" color="text.rprimary" fontWeight="medium" gutterBottom>
+            $ ralladito - no esta a la venta
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to cook
+            together with your guests. 
+          </Typography>
+          <hr color="#cecccc"></hr>
+          <Typography variant="body2" color="text.rprimary" fontWeight="medium"  gutterBottom>
+            Dias de visita
+          </Typography>
+        </CardContent>
+        
+        
+        <CardActions>
+        <Chip label="Lunes" color="secondary" />
+        <Chip label="Martes" />
+        <Chip label="Miercoles" />
+        <Chip label="Jueves" />
+        </CardActions>
+          
+        <Button  color="secondary">Reserve</Button>
+          
+      </Card>
+    </Box>   
     </div>
   );
 }
